fa203e00e90e942cfb10f7e3ca92597a61fd2e70
yellowtao <125220233@qq.com>
2020-09-19 08:48:31 +0800

    cli error

--- app/WebSocket/ChatModule.php
+++ app/WebSocket/ChatModule.php
@@ -2,62 +2,84 @@
 /**
  * This file is part of Swoft.
  *
- * @link     https://swoft.org
+ * @link https://swoft.org
  * @document https://swoft.org/docs
- * @contact  group@swoft.org
- * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ * @contact group@swoft.org
+ * @license https://github.com/swoft-cloud/swoft/blob/master/LICENSE
  */
 
 namespace App\WebSocket;
 
-use App\WebSocket\Chat\HomeController;
 use Swoft\Http\Message\Request;
-use Swoft\Session\Session;
-use Swoft\WebSocket\Server\Annotation\Mapping\OnOpen;
+use Swoft\Http\Message\Response;
 use Swoft\WebSocket\Server\Annotation\Mapping\WsModule;
-use Swoft\WebSocket\Server\MessageParser\JsonParser;
-use function basename;
-use function server;
+use Swoft\WebSocket\Server\Annotation\Mapping\OnOpen;
+use Swoft\WebSocket\Server\Annotation\Mapping\OnClose;
+use Swoft\WebSocket\Server\Annotation\Mapping\OnMessage;
+use Swoft\WebSocket\Server\Annotation\Mapping\OnHandshake;
+use Swoole\WebSocket\Frame;
+use Swoole\WebSocket\Server;
 
 /**
- * Class ChatModule
+ * Class ChatModule - This is an module for handle websocket, use custom message dispatch.
  *
- * @WsModule(
- *     "/chat",
- *     messageParser=JsonParser::class,
- *     controllers={HomeController::class}
- * )
+ * @WsModule("/chat")
  */
-class ChatModule
-{
+class ChatModule{
+    /**
+     * 在这里你可以验证握手的请求信息
+     * - 必须返回含有两个元素的array
+     *  - 第一个元素的值来决定是否进行握手
+     *  - 第二个元素是response对象
+     * - 可以在response设置一些自定义header,body等信息
+     *
+     * @OnHandshake()
+     * @param Request $request
+     * @param Response $response
+     * @return array
+     * [
+     *  self::HANDSHAKE_OK,
+     *  $response
+     * ]
+     */
+    public function checkHandshake(Request $request, Response $response): array
+    {
+        // some validate logic ...
+
+        return [true, $response];
+    }
+
     /**
      * @OnOpen()
+     * @param Server $server
      * @param Request $request
-     * @param int     $fd
+     * @param int $fd
+     * @return mixed
      */
-    public function onOpen(Request $request, int $fd): void
+    public function onOpen(Server $server, Request $request, int $fd)
     {
-        server()->push($request->getFd(), "Opened, welcome!(FD: $fd)");
+        // $server->push($fd, 'hello, welcome! :)');
+    }
 
-        $fullClass = Session::current()->getParserClass();
-        $className = basename($fullClass);
+    /**
+     * @OnMessage()
+     * @param Server $server
+     * @param Frame $frame
+     * @return mixed
+     */
+    public function onMessage(Server $server, Frame $frame)
+    {
+        // $server->push($frame->fd, 'hello, I have received your message: ' . $frame->data);
+    }
 
-        $help = <<<TXT
-Message data parser: $className
-Send message example:
-
-```json
-{
-    "cmd": "home.index",
-    "data": "hello"
-}
-```
-
-Description:
-
-- cmd `home.index` => App\WebSocket\Chat\HomeController::index()
-TXT;
-
-        server()->push($fd, $help);
+    /**
+     * @OnClose()
+     * @param Server $server
+     * @param int $fd
+     * @return mixed
+     */
+    public function onClose(Server $server, int $fd)
+    {
+        // do something. eg. record log
     }
 }
